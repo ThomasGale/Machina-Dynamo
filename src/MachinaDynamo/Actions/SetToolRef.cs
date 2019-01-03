6ceb6f19f265c63122891558d767aa5b38b7e0eb
Thomas Gale <thomas.gale@autodesk.com>
2019-01-03 16:11:35 +0000

    WIP: Adding the concept of a simple tool and workplane reference - this is because the DED cell has exisiting tool and workplane defintions defined in a global module (we don't want to have to define them again!)

---
+++ src/MachinaDynamo/Actions/SetToolRef.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.DesignScript.Runtime;
+using Autodesk.DesignScript.Interfaces;
+using Autodesk.DesignScript.Geometry;
+
+using Machina;
+using MAction = Machina.Action;
+using MPoint = Machina.Point;
+using MVector = Machina.Vector;
+using MOrientation = Machina.Orientation;
+using MTool = Machina.Tool;
+using System.IO;
+
+
+namespace MachinaDynamo
+{
+    //   █████╗ ████████╗████████╗ █████╗  ██████╗██╗  ██╗████████╗ ██████╗  ██████╗ ██╗     
+    //  ██╔══██╗╚══██╔══╝╚══██╔══╝██╔══██╗██╔════╝██║  ██║╚══██╔══╝██╔═══██╗██╔═══██╗██║     
+    //  ███████║   ██║      ██║   ███████║██║     ███████║   ██║   ██║   ██║██║   ██║██║     
+    //  ██╔══██║   ██║      ██║   ██╔══██║██║     ██╔══██║   ██║   ██║   ██║██║   ██║██║     
+    //  ██║  ██║   ██║      ██║   ██║  ██║╚██████╗██║  ██║   ██║   ╚██████╔╝╚██████╔╝███████╗
+    //  ╚═╝  ╚═╝   ╚═╝      ╚═╝   ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝   ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝
+    //                                                                           
+    partial class Actions
+    {
+        /// <summary>
+        /// Simply set the tool reference used for subsequent linear/joint motions in cartensian space
+        /// </summary>
+        /// <param name="toolName">The name of the referenced Tool which is already attached to the Robot flange.</param>
+        /// <returns name="Action">SetToolRef Action</returns>
+        public static MAction SetToolRef(string toolName)
+        {
+            if (toolName == "")
+            {
+                DynamoServices.LogWarningMessageEvents.OnLogWarningMessage("No tool name specified...");
+                return null;
+            }
+            return new ActionSetToolRef(toolName);
+        } 
+
+    }
+}
---
+++ src/MachinaDynamo/Actions/SetWorkplaneRef.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.DesignScript.Runtime;
+using Autodesk.DesignScript.Interfaces;
+using Autodesk.DesignScript.Geometry;
+
+using Machina;
+using MAction = Machina.Action;
+using MPoint = Machina.Point;
+using MVector = Machina.Vector;
+using MOrientation = Machina.Orientation;
+using MTool = Machina.Tool;
+using System.IO;
+
+
+namespace MachinaDynamo
+{
+    //   █████╗ ████████╗████████╗ █████╗  ██████╗██╗  ██╗████████╗ ██████╗  ██████╗ ██╗     
+    //  ██╔══██╗╚══██╔══╝╚══██╔══╝██╔══██╗██╔════╝██║  ██║╚══██╔══╝██╔═══██╗██╔═══██╗██║     
+    //  ███████║   ██║      ██║   ███████║██║     ███████║   ██║   ██║   ██║██║   ██║██║     
+    //  ██╔══██║   ██║      ██║   ██╔══██║██║     ██╔══██║   ██║   ██║   ██║██║   ██║██║     
+    //  ██║  ██║   ██║      ██║   ██║  ██║╚██████╗██║  ██║   ██║   ╚██████╔╝╚██████╔╝███████╗
+    //  ╚═╝  ╚═╝   ╚═╝      ╚═╝   ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝   ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝
+    //                                                                           
+    partial class Actions
+    {
+        /// <summary>
+        /// Simply set the workplane reference used for subsequent linear/joint motions in cartensian space
+        /// </summary>
+        /// <param name="workplaneName">The name of the referenced workplane (linear/joint motions in cartensian space will be with respect to this workplane).</param>
+        /// <returns name="Action">SetToolRef Action</returns>
+        public static MAction SetWorkplaneRef(string workplaneName)
+        {
+            if (workplaneName == "")
+            {
+                DynamoServices.LogWarningMessageEvents.OnLogWarningMessage("No workplane name specified...");
+                return null;
+            }
+            return new ActionSetWorkplaneRef(workplaneName);
+        } 
+
+    }
+}
